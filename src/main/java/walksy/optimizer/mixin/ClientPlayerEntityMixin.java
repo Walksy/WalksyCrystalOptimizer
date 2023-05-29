6e35c031746328f1b056871959ef4462a431a268
Walksy <walksy456@gmail.com>
2023-05-29 17:34:12 +0100

    Removed Non-Vanilla features

--- src/main/java/walksy/optimizer/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/walksy/optimizer/mixin/ClientPlayerEntityMixin.java
@@ -2,6 +2,7 @@ package walksy.optimizer.mixin;
 
 import net.minecraft.block.Blocks;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.decoration.EndCrystalEntity;
 import net.minecraft.entity.mob.MagmaCubeEntity;
 import net.minecraft.entity.mob.SlimeEntity;
@@ -30,45 +31,34 @@ public abstract class ClientPlayerEntityMixin {
      */
 
     private int hitCount;
-    private int stopAnomalyInt;
 
-    @Inject(at = @At(value = "INVOKE",
-            target = "Lnet/minecraft/client/network/AbstractClientPlayerEntity;tick()V",
-            ordinal = 0), method = "tick()V")
+    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/AbstractClientPlayerEntity;tick()V", ordinal = 0), method = "tick()V")
     private void useOwnTicks(CallbackInfo ci) {
-        //Used a tickMethod as it's generally faster than sending packets through a ClientConnectionMixin
-        if (!EnableOptimizerCommand.fastCrystal)
-            return;
-        if (!mc.options.useKey.isPressed()) {
-            hitCount = 0;
-        }
-        if (!mc.options.attackKey.isPressed()) {
-            stopAnomalyInt = 0;
-        }
-        if (hitCount == PacketUtils.shouldStopInt()) {
-            return;
-        }
-        ItemStack mainHandStack = mc.player.getMainHandStack();
-        if (!mainHandStack.isOf(Items.END_CRYSTAL))
-            return;
-        if (mc.crosshairTarget instanceof EntityHitResult entity) {
-            if (entity.getEntity() instanceof EndCrystalEntity
-                    || entity.getEntity() instanceof MagmaCubeEntity //checks magma cubes so the tracking int is counting attacks on servers like donutsmp and eastpvp
-                    || entity.getEntity() instanceof SlimeEntity) //same reason for magma
-            {
-                if (mc.options.attackKey.isPressed()) {
-                    if (stopAnomalyInt == 1) return; //sometimes the int is counted more than once, this stops that
+        if (EnableOptimizerCommand.fastCrystal) {
+            boolean isUseKeyPressed = mc.options.useKey.isPressed();
+            boolean isAttackKeyPressed = mc.options.attackKey.isPressed();
+            int stopInt = PacketUtils.shouldStopInt();
+            ItemStack mainHandStack = mc.player.getMainHandStack();
+            Vec3d camPos = mc.player.getEyePos();
+            Vec3d clientLookVec = PacketUtils.getClientLookVec();
+            BlockHitResult lookPos = mc.world.raycast(new RaycastContext(camPos, camPos.add(clientLookVec.multiply(4.5)), RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.NONE, mc.player));
+            if (isAttackKeyPressed && PacketUtils.isBlock(Blocks.OBSIDIAN,lookPos.getBlockPos()))
+                return;
+            if (!isUseKeyPressed) {
+                hitCount = 0;
+            }
+            if (hitCount == stopInt) {
+                return;
+            }
+            if (!mainHandStack.isOf(Items.END_CRYSTAL)) {
+                return;
+            }
+            if (mc.crosshairTarget instanceof EntityHitResult entity && (entity.getEntity() instanceof EndCrystalEntity crystal || entity.getEntity() instanceof MagmaCubeEntity magma || entity.getEntity() instanceof SlimeEntity slime)) {
+                if (isAttackKeyPressed) {
                     hitCount++;
-                    stopAnomalyInt++;
-                    //records the amount of times a crystal is destroyed by the player
-                    //making sure crystals don't stack by limiting sent packets
                 }
             }
-        }
-        Vec3d camPos = mc.player.getEyePos();
-        BlockHitResult lookPos = mc.world.raycast(new RaycastContext(camPos, camPos.add(PacketUtils.getClientLookVec().multiply(4.5)), RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.NONE, mc.player));
-        if (mc.options.useKey.isPressed()) {
-            if (PacketUtils.isLookingAt(Blocks.OBSIDIAN, lookPos.getBlockPos()) || PacketUtils.isLookingAt(Blocks.BEDROCK, lookPos.getBlockPos())) {
+            if (isUseKeyPressed && (PacketUtils.isLookingAt(Blocks.OBSIDIAN, lookPos.getBlockPos()) || PacketUtils.isLookingAt(Blocks.BEDROCK, lookPos.getBlockPos()))) {
                 ActionResult result = mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND, lookPos);
                 if (result.isAccepted() && result.shouldSwingHand()) {
                     PacketUtils.interact(lookPos.getBlockPos(), lookPos.getSide());
